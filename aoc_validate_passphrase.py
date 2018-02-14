876c0c05c003752b7fb30374b32f1f8fd36ae855
mc_vibraphone <tom.mcmanus@gmail.com>
2018-02-13 20:19:34 -0500

    Added anagram check

--- aoc_validate_passphrase.py
+++ aoc_validate_passphrase.py
@@ -3,8 +3,23 @@
 def validate_passphrase(phrase):
     bits = set()
     for bit in phrase.split():
+        bit = ''.join(sorted(bit))
         print(bit)
         if bit in bits:
             return False
         bits.add(bit)
     return True
+
+
+if __name__ == '__main__':
+    with open('./test_data', 'r') as f:
+        phrases = f.readlines()
+
+    count = 0
+    for p in phrases:
+        print('Checking {}'.format(p))
+        if validate_passphrase(p):
+            count = count + 1
+
+    print('Valid count was {}'.format(count))
+
